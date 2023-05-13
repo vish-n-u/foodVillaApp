212d7a8f7dd18434577d687da4d2df124abe982b
vish-n-u <vishnuna26@gmail.com>
2023-05-13 09:48:50 +0530

    added new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./Authorization/src/**/*.{html,js,jsx}",
+    "./src/**/*.{html,js,jsx}",
+  ],
   theme: {
     extend: {},
   },
